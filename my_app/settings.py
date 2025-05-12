8490efbd17d2556e65b7e0b845a218411aa9dcc4
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-05-12 20:37:09 +0700

    - revert sqlite path

--- my_app/settings.py
+++ my_app/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'my_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'my_app/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
